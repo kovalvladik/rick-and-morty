ae98df0eb8b5307dc8ad27e1043e787591191624
Vladislav <kovalvlad9@gmail.com>
2021-08-04 14:47:48 +0300

    add images for main page

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,12 +2,27 @@ import {Button, Card, CardHeader, Grid, makeStyles} from "@material-ui/core";
 import CardMedia from "@material-ui/core/CardMedia";
 import {Link} from "react-router-dom";
 import {CharacterProfile} from "../components/CharacterProfile";
+import {logo1} from '../images/logo1.png'
+import {logo2} from '../images/logo2.jpeg'
+import {logo3} from '../images/logo3.jpeg'
 
 
 const useStyles = makeStyles({
     root:{
         minHeight: 'calc(100vh - 64px - 135px)',
-    }
+
+    },
+    item:{
+        maxWidth:'300px',
+        maxHeight:'500px',
+        margin:'2rem',
+    },
+    card:{
+        maxHeight:'500px',
+        minHeight:'500px',
+
+    },
+
 })
 
 function Main () {
@@ -18,30 +33,33 @@ function Main () {
 
 
     return< Grid container className={classes.root} spacing={3} >
-      <Grid item>
-        <Card>
+      <Grid item  className={classes.item}>
+        <Card className={classes.card}>
             <CardHeader title='Characters'/>
-            <CardMedia >
+            <CardMedia  >
+                <img style={{maxHeight:'400px', minHeight:'400px'}} src='https://static1.colliderimages.com/wordpress/wp-content/uploads/2021/05/New-Rick-and-Morty-trailer.jpg?q=50&fit=contain&w=750&h=375&dpr=1.5'/>
                 <Button component={Link} to={`/allCharacter`}>
                     push
                 </Button>
             </CardMedia>
         </Card>
       </Grid>
-      <Grid item>
-          <Card>
+      <Grid item className={classes.item}>
+          <Card className={classes.card}>
               <CardHeader title='Episode'/>
               <CardMedia >
+                  <img style={{maxHeight:'400px'}} src='https://www.kindpng.com/picc/m/376-3760724_rick-and-morty-png-download-image-rick-and.png'/>
                 <Button component={Link} to={`/allEpisode`}>
                     push
                 </Button>
               </CardMedia>
           </Card>
       </Grid>
-      <Grid item>
-          <Card>
-              <CardHeader title='Location'/>
-              <CardMedia >
+      <Grid item  className={classes.item}>
+          <Card className={classes.card}>
+              <CardHeader title='Location' />
+              <CardMedia  >
+                  <img style={{maxHeight:'400px'}} src='https://www.denofgeek.com/wp-content/uploads/2021/03/Rick-and-Morty-Season-5-Trailer.jpg?resize=768%2C432'/>
                   <Button component={Link} to={`/allLocation`}>
                       push
                   </Button>
