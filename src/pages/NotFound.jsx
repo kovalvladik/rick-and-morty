4dcb328055f96f9cb09f7c6da6468b0ea0c44cab
Vladislav <kovalvlad9@gmail.com>
2021-08-28 07:02:08 +0300

    refactor not found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,19 +1,36 @@
-import {makeStyles} from "@material-ui/core";
+import {Button, Card, CardHeader, CardMedia, List, makeStyles, Typography} from "@material-ui/core";
+import {Link} from "react-router-dom";
 
 const useStyles = makeStyles({
     root: {
         minHeight: 'calc(100vh - 64px - 116px)',
 
     },
+    text: {
+        padding: '3rem',
 
+    },
+    media:{
+      display: 'flex',
+      position:'relative',
+    },
 
 })
 
 function NotFound() {
     const classes = useStyles()
-    return <main className={classes.root}>
-        nothing here
-    </main>
+    return <Card className={classes.root}>
+            <Typography align='center' component='h1' className={classes.text}>
+                It's not this page for your looking...
+            </Typography>
+        <CardMedia className={classes.media} >
+            <img style={{maxHeight: '400px'}}
+                 src='https://www.denofgeek.com/wp-content/uploads/2021/03/Rick-and-Morty-Season-5-Trailer.jpg?resize=768%2C432'/>
+            <Button component={Link} to={`/`}>
+                Go Home
+            </Button>
+        </CardMedia>
+    </Card>
 }
 
 export {NotFound}
